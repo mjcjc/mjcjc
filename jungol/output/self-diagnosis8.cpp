3a1ac2530062557e8fb9590f0113879daba433ec
mjcjc <67304624+mjcjc@users.noreply.github.com>
2022-01-12 02:43:38 +0900

    Create self-diagnosis8.cpp

---
+++ jungol/output/self-diagnosis8.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main() {
+
+	std::printf("%10s%10s%10s\n","item","count","price");
+	std::printf("%10s%10d%10d\n", "pen", 20, 100);
+	std::printf("%10s%10d%10d\n", "note", 5, 95);
+	std::printf("%10s%10d%10d\n", "eraser", 110, 97);
+
+	return 0;
+}

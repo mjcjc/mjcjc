9e3b69bda3254e33ff889afbe84f28886088c7ef
mjcjc <67304624+mjcjc@users.noreply.github.com>
2022-01-12 02:52:19 +0900

    Create formative evaluation3.cpp

---
+++ jungol/output/formative evaluation3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main() {
+
+	std::cout << "TTTTTTTTTT" << std::endl;
+	std::cout << "TTTTTTTTTT" << std::endl;
+	std::printf("%6s\n", "TT");
+	std::printf("%6s\n", "TT");
+	std::printf("%6s", "TT");
+	return 0;
+}

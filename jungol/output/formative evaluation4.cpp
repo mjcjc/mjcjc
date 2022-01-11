51e86a7c27167b00cc8600525d378decc5005ad8
mjcjc <67304624+mjcjc@users.noreply.github.com>
2022-01-12 02:55:50 +0900

    Create formative evaluation4.cpp

---
+++ jungol/output/formative evaluation4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+int main() {
+	int k = 90;
+	int m = 80;
+	int e = 100;
+	std::printf("kor %d\n", k);
+	std::printf("mat %d\n", m);
+	std::printf("eng %d\n", e);
+	std::printf("sum %d\n", k + m + e);
+	std::printf("avg %d", (k + m + e) / 3);
+	return 0;
+}
